e4c4bdb1c755f07b3e154bd24a0ad93afecd3cef
Ruba-Salih <rubasalih900@gmail.com>
2023-11-14 03:38:11 -0800

    project

--- handle_int.c
+++ handle_int.c
@@ -26,7 +26,8 @@ int number_of_digits(int num)
  */
 int handle_integer(va_list inf)
 {
-	int num, len, i, tmp, arr[];
+	int num, len, i;
+        char * arr;
 
 	num = va_arg(inf, int);
 	len = number_of_digits(num);
@@ -38,18 +39,17 @@ int handle_integer(va_list inf)
 		num = -num;
 	}
 
-	arr = malloc(sizeof(int) * len);
+	arr = malloc(sizeof(char) * len);
 	while (num != 0)
 	{
-		arr[i] = num % 10;
+		arr[i] = '0' + (num % 10);
 		num /= 10;
 		i++;
 	}
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		tmp = arr[i];
-		fill(tmp + '0');
+		fill(arr[i]);
 	}
 	free(arr);
 	return (len);
